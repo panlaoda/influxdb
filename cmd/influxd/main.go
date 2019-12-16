aec8be97d02af3a51b1a50c8690b24f25240b43c
Jacob Marble <jacobmarble@influxdata.com>
2019-12-16 10:51:50 -0800

    feat(backup): initial restore work

--- cmd/influxd/main.go
+++ cmd/influxd/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/influxdata/influxdb/cmd/influxd/generate"
 	"github.com/influxdata/influxdb/cmd/influxd/inspect"
 	"github.com/influxdata/influxdb/cmd/influxd/launcher"
+	"github.com/influxdata/influxdb/cmd/influxd/restore"
 	_ "github.com/influxdata/influxdb/query/builtin"
 	_ "github.com/influxdata/influxdb/tsdb/tsi1"
 	_ "github.com/influxdata/influxdb/tsdb/tsm1"
@@ -37,6 +38,7 @@ func init() {
 	rootCmd.AddCommand(launcher.NewCommand())
 	rootCmd.AddCommand(generate.Command)
 	rootCmd.AddCommand(inspect.NewCommand())
+	rootCmd.AddCommand(restore.Command)
 }
 
 // find determines the default behavior when running influxd.
