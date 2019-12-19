ab219720bb49059dda0037f7e55b0f19e629727e
Edd Robinson <me@edd.io>
2019-12-19 13:22:43 +0000

    feat: wip

--- cmd/influxd/main.go
+++ cmd/influxd/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"net/http"
 	_ "net/http/pprof"
 	"os"
+	"runtime"
 	"strings"
+	"time"
 
 	"github.com/influxdata/influxdb"
 	"github.com/influxdata/influxdb/cmd/influxd/generate"
 	"github.com/influxdata/influxdb/cmd/influxd/inspect"
 	"github.com/influxdata/influxdb/cmd/influxd/launcher"
+	"github.com/influxdata/influxdb/kit/cli"
 	_ "github.com/influxdata/influxdb/query/builtin"
 	_ "github.com/influxdata/influxdb/tsdb/tsi1"
 	_ "github.com/influxdata/influxdb/tsdb/tsm1"
@@ -27,12 +31,24 @@ var rootCmd = &cobra.Command{
 	Short: "Influx Server",
 }
 
+var enableDebugProfiling bool
+
 func init() {
 	influxdb.SetBuildInfo(version, commit, date)
 	viper.SetEnvPrefix("INFLUXD")
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	rootCmd.InitDefaultHelpCmd()
+
+	cli.BindOptions(rootCmd, []cli.Opt{
+		{
+			DestP:      &enableDebugProfiling,
+			Flag:       "enable-debug-pprof",
+			Default:    false,
+			Desc:       "setting this enabled profiles to be taken from server initialisation on port 6060",
+			Persistent: true,
+		},
+	})
 
 	rootCmd.AddCommand(launcher.NewCommand())
 	rootCmd.AddCommand(generate.Command)
@@ -53,6 +69,12 @@ func find(args []string) *cobra.Command {
 }
 
 func main() {
+	if enableDebugProfiling {
+		runtime.SetBlockProfileRate(int(time.Second))
+		runtime.SetMutexProfileFraction(1)
+		go func() { http.ListenAndServe("localhost:6060", nil) }()
+	}
+
 	cmd := find(os.Args[1:])
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
